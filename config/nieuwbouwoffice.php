64a7b9aebd723341f1791e6de379c1a2f86a290c
Jan Henk Hazelaar <janhenk@concept7.nl>
2026-05-05 20:53:59 +0200

    Set up initial package and integrate nbo-php-sdk
    Rename skeleton scaffolding to NieuwbouwOffice, remove views/migrations/
    factories/commands the package doesn't need, ignore .DS_Store, and pull
    in concept7/nbo-php-sdk as the underlying API client.

---
+++ config/nieuwbouwoffice.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'api_token' => env('NIEUWBOUWOFFICE_API_TOKEN', ''),
+
+    'base_url' => env('NIEUWBOUWOFFICE_API_URL', 'https://api.nbo.nl/rest'),
+
+];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,37 +1,21 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Concept7\LaravelNieuwbouwOffice\Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use Concept7\LaravelNieuwbouwOffice\NieuwbouwOfficeServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use VendorName\Skeleton\SkeletonServiceProvider;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
-            SkeletonServiceProvider::class,
+            NieuwbouwOfficeServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-         foreach (\Illuminate\Support\Facades\File::allFiles(__DIR__ . '/../database/migrations') as $migration) {
-            (include $migration->getRealPath())->up();
-         }
-         */
     }
 }
